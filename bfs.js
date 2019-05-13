3163dfac55bf1ad9125cfb2ae32c2385db70cb0e
Kyle <kyle1james@gmail.com>
2019-05-13 07:57:47 -0400

    meow

--- bfs.js
+++ bfs.js
@@ -1,9 +1,12 @@
 var cities = [];
-
-
+var visited = {};
+var parent = {};
 
 function setup() {
   createCanvas(800, 500);
+  background(51);
+
+
   for (var i = 0; i < 8; i++){
       x = random(0,800);
       y = random(0,500);
@@ -11,23 +14,34 @@ function setup() {
       cities[i] = new Node(x,y, edges);
     }
 
-
     for (x of cities){
       x.draw();
       x.map_draw(cities);
     }
-
 }
 
-// cities.sort(sortMe);
-//
-// function sortMe(a, b)
-// {
-//   return (b.position[0] - a.position[0]) || (b.position[1] - a.position[1]);
-// }
-
 function draw(){
+  let start = cities[0]
+  var path = {}
+  let queue = [cities[0]]
 
+  while (queue.length > 0){
+    let parent_node = queue.pop(0);
+    for (x in parent_node.adj_list){
+      let current_weight = start.distance(x)
+      let dist = parent_node.distance(x) + current_weight
+      if (dist < cities[x] || !(x in visited)){
+        queue.push(x);
+        visited[x] = dist;
+        parent[x] = parent_node
+      }
+
+    }
+  }
+  for (x in parent){
+    console.log(x)
+
+  }
   // stroke(255,0,255);
   // strokeWeight(1);
   // noFill()
